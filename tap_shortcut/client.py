e52f075258d067dbf71e7dca8afa46a670023ffe
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-07-31 13:41:06 -0600

    refactor: Use the schema source feature from the Meltano Singer SDK

--- tap_shortcut/client.py
+++ tap_shortcut/client.py
@@ -2,16 +2,50 @@
 
 from __future__ import annotations
 
+import importlib.resources
 import sys
 import typing as t
+from copy import deepcopy
 
-from singer_sdk import RESTStream
+from singer_sdk import OpenAPISchema, RESTStream, StreamSchema
 from singer_sdk.authenticators import APIKeyAuthenticator
+from toolz.dicttoolz import get_in
 
 if sys.version_info >= (3, 12):
     from typing import override
 else:
     from typing_extensions import override
+
+SPEC = importlib.resources.files("tap_shortcut") / "openapi.json"
+
+
+def handle_x_nullable(schema: dict[str, t.Any]) -> dict[str, t.Any]:
+    """Resolve x-nullable properties to standard JSON Schema nullable type.
+
+    Args:
+        schema: A JSON Schema dictionary.
+
+    Returns:
+        A new JSON Schema dictionary with 'x-nullable' resolved to [<type>, "null"].
+    """
+    result = deepcopy(schema)
+
+    if "object" in result["type"]:
+        for prop, prop_schema in result.get("properties", {}).items():
+            if prop_schema.get("x-nullable", False):
+                prop_type: str | list[str] = prop_schema.get("type", [])
+                types = [prop_type] if isinstance(prop_type, str) else prop_type
+                prop_schema["type"] = [*types, "null"]
+
+            result["properties"][prop] = handle_x_nullable(prop_schema)
+
+    elif "array" in result["type"]:
+        result["items"] = handle_x_nullable(result["items"])
+
+    if "enum" in result and None not in result["enum"]:
+        result["enum"].append(None)
+
+    return result
 
 
 def _handle_runtime_nullables(
@@ -26,12 +60,56 @@ def _handle_runtime_nullables(
             schema["properties"][field]["type"] = [*types, "null"]
 
 
+class ResponseKey(t.NamedTuple):
+    """Response key."""
+
+    path: str
+    http_method: str
+    expected_status: int = 200
+
+
+class ShortcutOpenAPISchema(OpenAPISchema[ResponseKey]):
+    """Shortcut OpenAPI schema."""
+
+    @override
+    def get_unresolved_schema(self, key: ResponseKey) -> dict[str, t.Any]:
+        return get_in(  # type: ignore[no-any-return]
+            keys=(
+                "paths",
+                key.path,
+                key.http_method.lower(),
+                "responses",
+                str(key.expected_status),
+                "schema",
+                "items",
+            ),
+            coll=self.spec,
+        )
+
+
+class ShortcutSchema(StreamSchema[ResponseKey]):
+    """Shortcut schema."""
+
+    @override
+    def get_stream_schema(
+        self,
+        stream: ShortcutStream,  # type: ignore[override]
+        stream_class: type[ShortcutStream],  # type: ignore[override]
+    ) -> dict[str, t.Any]:
+        key = ResponseKey(stream.path, stream.http_method)
+        schema = handle_x_nullable(self.schema_source.fetch_schema(key))
+        _handle_runtime_nullables(schema, stream.extra_nullable_fields)
+        stream_class.preprocess_schema(schema)
+        return schema
+
+
 class ShortcutStream(RESTStream[None]):
     """Shortcut stream class."""
 
     url_base = "https://api.app.shortcut.com"
     records_jsonpath = "$[*]"
     primary_keys: t.ClassVar[list[str]] = ["id"]
+    schema: t.ClassVar[ShortcutSchema] = ShortcutSchema(ShortcutOpenAPISchema(SPEC))
 
     # Abstract fields
     extra_nullable_fields: tuple[str, ...] = ()
--- tap_shortcut/tap.py
+++ tap_shortcut/tap.py
@@ -2,16 +2,10 @@
 
 from __future__ import annotations
 
-import importlib.resources
-import json
 import sys
-from copy import deepcopy
-from typing import TYPE_CHECKING, Any
 
-from singer_sdk import RESTStream, Stream, Tap
+from singer_sdk import Stream, Tap
 from singer_sdk import typing as th
-from singer_sdk.singerlib import resolve_schema_references
-from toolz.dicttoolz import get_in
 
 from tap_shortcut import streams
 
@@ -19,58 +13,6 @@ if sys.version_info >= (3, 12):
     from typing import override
 else:
     from typing_extensions import override
-
-
-if TYPE_CHECKING:
-    from tap_shortcut.client import ShortcutStream
-
-STREAM_TYPES: list[type[ShortcutStream]] = [
-    streams.Categories,
-    streams.Epics,
-    streams.Files,
-    streams.Groups,
-    streams.Iterations,
-    streams.Labels,
-    streams.Members,
-    streams.Milestones,
-    streams.Projects,
-    streams.ProjectStories,
-    streams.Repositories,
-    streams.Workflows,
-]
-
-SPEC = importlib.resources.files("tap_shortcut") / "openapi.json"
-
-
-def handle_x_nullable(schema: dict[str, Any]) -> dict[str, Any]:
-    """Resolve x-nullable properties to standard JSON Schema nullable type.
-
-    Args:
-        schema: A JSON Schema dictionary.
-
-    Returns:
-        A new JSON Schema dictionary with 'x-nullable' resolved to [<type>, "null"].
-    """
-    result = deepcopy(schema)
-
-    if "object" in result["type"]:
-        for prop, prop_schema in result.get("properties", {}).items():
-            prop_type: str | list[str] = prop_schema.get("type", [])
-            types = [prop_type] if isinstance(prop_type, str) else prop_type
-            nullable: bool = prop_schema.get("x-nullable", False)
-
-            if nullable:
-                prop_schema["type"] = [*types, "null"]
-
-            result["properties"][prop] = handle_x_nullable(prop_schema)
-
-    elif "array" in result["type"]:
-        result["items"] = handle_x_nullable(result["items"])
-
-    if "enum" in result and None not in result["enum"]:
-        result["enum"].append(None)
-
-    return result
 
 
 class TapShortcut(Tap):
@@ -87,42 +29,20 @@ class TapShortcut(Tap):
         ),
     ).to_dict()
 
-    def get_openapi_schema(self) -> dict[Any, Any]:
-        """Retrieve Swagger/OpenAPI schema for this API.
-
-        Returns:
-            OpenAPI schema.
-
-        Raises:
-            RuntimeError: If the OpenAPI schema cannot be retrieved.
-        """
-        with SPEC.open() as spec:
-            return json.load(spec)  # type: ignore[no-any-return]
-
     @override
     def discover_streams(self) -> list[Stream]:
         """Return a list of discovered streams."""
-        openapi_schema = self.get_openapi_schema()
-        streams: list[RESTStream[None]] = []
-
-        for stream_type in STREAM_TYPES:
-            schema = get_in(
-                keys=[
-                    "paths",
-                    stream_type.path,  # ty: ignore[unresolved-attribute]
-                    "get",
-                    "responses",
-                    "200",
-                    "schema",
-                    "items",
-                ],
-                coll=openapi_schema,
-            )
-            schema["definitions"] = openapi_schema["definitions"]
-            resolved_schema = resolve_schema_references(schema)
-            clean_schema = handle_x_nullable(resolved_schema)
-            stream_type.preprocess_schema(clean_schema)
-
-            streams.append(stream_type(tap=self, schema=clean_schema))
-
-        return sorted(streams, key=lambda x: x.name)
+        return [
+            streams.Categories(tap=self),
+            streams.Epics(tap=self),
+            streams.Files(tap=self),
+            streams.Groups(tap=self),
+            streams.Iterations(tap=self),
+            streams.Labels(tap=self),
+            streams.Members(tap=self),
+            streams.Milestones(tap=self),
+            streams.Projects(tap=self),
+            streams.ProjectStories(tap=self),
+            streams.Repositories(tap=self),
+            streams.Workflows(tap=self),
+        ]
